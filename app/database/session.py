7908eebf48d78051b965606ea95d9ed35537c7c6
jt201 <201@studio-jt.co.kr>
2023-12-07 13:24:51 +0900

    sqlite 경로 수정

--- app/database/session.py
+++ app/database/session.py
@@ -1,12 +1,18 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, Session
 
 from app.database.context import get_db_session_context
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///app/database/migrations/db.sqlite"
+SQLALCHEMY_DATABASE_URL = "sqlite:///" + os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), "migrations", "db.sqlite"
+)
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True
 )
 session_factory = sessionmaker(autoflush=False, autocommit=False, bind=engine)
-session: Session | scoped_session = scoped_session(session_factory=session_factory, scopefunc=get_db_session_context)
+session: Session | scoped_session = scoped_session(
+    session_factory=session_factory, scopefunc=get_db_session_context
+)
