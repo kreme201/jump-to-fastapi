799de45f68110a97903ec10ab560afd4a37dc6c8
jt201 <201@studio-jt.co.kr>
2023-12-07 17:14:39 +0900

    schema 로드 처리 수정

--- app/schemas.py
+++ app/schemas.py
@@ -1,25 +1,7 @@
-import importlib
-import os
-
 from app.database.base import Base
+from app.pybo import schemas as pybo_schemas
 
-EXCLUDE_FOLDERS = [
-    "__pycache__",  # Python Cache Folder
-    "database",  # Database Configurations
+__all__ = [
+    Base,
+    pybo_schemas,
 ]
-
-base_path = os.path.dirname(__file__)
-package_name = os.path.basename(os.path.normpath(base_path))
-for root, dirs, files in os.walk(base_path):
-    for dir_item in dirs:
-        if dir_item in EXCLUDE_FOLDERS:
-            continue
-
-        schemas_module_name = f"{package_name}.{root[len(base_path) + 1:].replace(os.path.sep, '.')}.{dir_item}.schemas"
-
-        try:
-            schemas_module = importlib.import_module(schemas_module_name)
-        except ImportError as e:
-            pass
-
-__all__ = [Base]
