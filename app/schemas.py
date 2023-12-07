cf3f85f2e65ce421e11da781495ee1b29af12bb5
jt201 <201@studio-jt.co.kr>
2023-12-07 10:21:17 +0900

    table 에 pybo 접두어 추가

--- app/schemas.py
+++ app/schemas.py
@@ -5,7 +5,7 @@ from app.database import Base
 
 
 class Question(Base):
-    __tablename__ = "question"
+    __tablename__ = "pybo_question"
 
     id = Column(Integer, primary_key=True)
     subject = Column(String, nullable=False)
@@ -14,10 +14,10 @@ class Question(Base):
 
 
 class Answer(Base):
-    __tablename__ = "answer"
+    __tablename__ = "pybo_answer"
 
     id = Column(Integer, primary_key=True)
     content = Column(Text, nullable=False)
     create_date = Column(DateTime, nullable=False)
-    question_id = Column(Integer, ForeignKey("question.id"))
+    question_id = Column(Integer, ForeignKey("pybo_question.id"))
     question = relationship("Question", backref="answers")
